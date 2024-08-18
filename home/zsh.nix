306132e580e3df59e6f7909e1fb011797d85eeeb
Buddha Christ <budchris@solrey3.com>
2024-08-18 19:48:35 -0400

    merge shell into zsh

--- home/zsh.nix
+++ home/zsh.nix
@@ -11,6 +11,8 @@
     initExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
       alias k="kubectl"
+      alias urldecode="python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'"
+      alias urlencode="python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'"
       alias hist="history 1"
       alias p2="cd ~/Nextcloud/obsidian/player2; nvim todo.md"
       alias dtf="cd ~/dotfiles; nvim"
