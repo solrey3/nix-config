18e17f3b9af932b31abc51c9342e1a8660efddb1
Buddha Christ <budchris@solrey3.com>
2024-08-18 22:12:59 -0400

    add delta ssh key to bravo

--- hosts/bravo/configuration.nix
+++ hosts/bravo/configuration.nix
@@ -98,6 +98,7 @@
       pasystray
     ];
     openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINNTDemhkFYx8kw6p096XBVp7H2gnONZLMX+4uDgwue/ budchris@delta"
     ];
   };
 
