1044f3ad5c7053b6926e23aa558cf6e2faa3761d
Buddha Christ <budchris@solrey3.com>
2024-08-18 23:23:49 -0400

    make nvidia modules for nixos

--- flake.nix
+++ flake.nix
@@ -65,6 +65,7 @@
         system = "x86_64-linux";
         modules = [
           ./hosts/alpha/configuration.nix
+          ./modules/nvidia-legacy_470.nix
           # make home-manager as a module of nixos
           # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
           home-manager.nixosModules.home-manager
@@ -87,6 +88,7 @@
         system = "x86_64-linux";
         modules = [
           ./hosts/bravo/configuration.nix
+          ./modules/nvidia-stable.nix
           # make home-manager as a module of nixos
           # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
           home-manager.nixosModules.home-manager
