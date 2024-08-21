26081313a3333a36c980aa2ae7a569291d37eeec
Buddha Christ <budchris@solrey3.com>
2024-08-20 20:52:37 -0400

    hyprland attempt 1 failed

--- modules/nixos-hyprland.nix
+++ modules/nixos-hyprland.nix
@@ -14,6 +14,29 @@
     xwayland.enable = true;
   };
 
+  environment.sessionVariables = {
+    # If your cursor becomes invisible
+    WLR_NO_HARDWARE_CURSORS = "1";
+    # Hint election apps to use wayland
+    NIXOS_OZONE_WL = "1";
+  };
+
+  environment.systemPackages = with pkgs; [
+    waybar
+    dunst
+    libnotify
+    swww
+    kitty
+    alacritty
+    rofi-wayland
+  ];
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "budchris";
+
   # Install firefox. ??Should this be here??
   programs.firefox.enable = true;
 
