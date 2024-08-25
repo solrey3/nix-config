4e2d48393c767c53dde21b5809bf24f5f74cc349
Buddha Christ <budchris@solrey3.com>
2024-08-25 12:15:38 -0400

    make starship more catpuccin

--- home/starship.nix
+++ home/starship.nix
@@ -1,19 +1,18 @@
-
 { ... }: {
   # starship - The minimal, blazing-fast, and infinitely customizable prompt for any shell!
   programs.starship = {
     enable = true;
     settings = {
-      format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)$hostname[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$nodejs$rust$golang$php$gcloud$conda[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)\n$character";
+      format = "[░▒▓](#a6adc8)[  ](bg:#a6adc8 fg:#1e1e2e)$hostname[](bg:#89b4fa fg:#a6adc8)$directory[](fg:#89b4fa bg:#585b70)$git_branch$git_status[](fg:#585b70 bg:#45475a)$nodejs$rust$golang$php$gcloud$conda[](fg:#45475a bg:#313244)$time[ ](fg:#313244)\n$character";
 
       hostname = {
         ssh_only = false;
-        style = "bg:#a3aed2 fg:#090c0c";
+        style = "bg:#a6adc8 fg:#1e1e2e";
         format = "[$hostname]($style)";
       };
 
       directory = {
-        style = "fg:#e3e5e5 bg:#769ff0";
+        style = "fg:#f5e0dc bg:#89b4fa";
         format = "[ $path ]($style)";
         truncation_length = 3;
         truncation_symbol = "…/";
@@ -27,58 +26,59 @@
 
       git_branch = {
         symbol = "";
-        style = "bg:#394260";
-        format = "[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)";
+        style = "bg:#585b70";
+        format = "[[ $symbol $branch ](fg:#89b4fa bg:#585b70)]($style)";
       };
 
       git_status = {
-        style = "bg:#394260";
-        format = "[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)";
+        style = "bg:#585b70";
+        format = "[[($all_status$ahead_behind )](fg:#89b4fa bg:#585b70)]($style)";
       };
 
       nodejs = {
         symbol = "";
-        style = "bg:#212736";
-        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+        style = "bg:#45475a";
+        format = "[[ $symbol ($version) ](fg:#89b4fa bg:#45475a)]($style)";
       };
 
       rust = {
         symbol = "";
-        style = "bg:#212736";
-        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+        style = "bg:#45475a";
+        format = "[[ $symbol ($version) ](fg:#89b4fa bg:#45475a)]($style)";
       };
 
       golang = {
         symbol = "";
-        style = "bg:#212736";
-        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+        style = "bg:#45475a";
+        format = "[[ $symbol ($version) ](fg:#89b4fa bg:#45475a)]($style)";
       };
 
       php = {
         symbol = "";
-        style = "bg:#212736";
-        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+        style = "bg:#45475a";
+        format = "[[ $symbol ($version) ](fg:#89b4fa bg:#45475a)]($style)";
       };
 
       gcloud = {
         symbol = "";
-        style = "bg:#212736";
-        format = "[[ $symbol ($account@$project) ](fg:#769ff0 bg:#212736)]($style)";
+        style = "bg:#45475a";
+        format = "[[ $symbol ($account@$project) ](fg:#89b4fa bg:#45475a)]($style)";
       };
 
       conda = {
         symbol = "";
-        style = "bg:#212736";
-        format = "[[ $symbol $environment ](fg:#769ff0 bg:#212736)]($style)";
+        style = "bg:#45475a";
+        format = "[[ $symbol $environment ](fg:#89b4fa bg:#45475a)]($style)";
       };
 
       time = {
         disabled = false;
         time_format = "%F %T";
-        style = "bg:#1d2230";
-        format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
+        style = "bg:#313244";
+        format = "[[  $time ](fg:#cdd6f4 bg:#313244)]($style)";
       };
     };
   };
 
 }
+
