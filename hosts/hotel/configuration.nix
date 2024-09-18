deabf411357f79ecc1162ffca1a32ace53294cb1
Buddha Christ <budchris@solrey3.com>
2024-09-18 17:07:15 -0400

    update hotel config1

--- hosts/hotel/configuration.nix
+++ hosts/hotel/configuration.nix
@@ -11,12 +11,12 @@
     ];
 
   # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
+  # boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/vda"; # or "nodev" for efi only
+  # boot.loader.grub.device = "/dev/vda"; # or "nodev" for efi only
 
   networking.hostName = "ubuntu2nixos"; # Define your hostname.
   # Pick only one of the below networking options.
