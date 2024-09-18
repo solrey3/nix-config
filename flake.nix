fdf8b973946a84b42ceecf0da2b5a51ebc288eb6
Buddha Christ <budchris@solrey3.com>
2024-09-17 23:13:04 -0400

    rename golf to digitalocean

--- flake.nix
+++ flake.nix
@@ -132,7 +132,7 @@
       };
 
       # Configuration for Digital Ocean droplets 
-      golf = nixpkgs-unstable.lib.nixosSystem {
+      digitalocean = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           disko.nixosModules.disko
@@ -147,25 +147,7 @@
 
             };
           }
-          ./hosts/golf/configuration.nix
-          ./modules/nixos/base.nix
-          stylix.nixosModules.stylix
-          ./modules/stylix.nix
-          # make home-manager as a module of nixos
-          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            # TODO replace ryan with your own username
-            home-manager.users.budchris = {
-              imports = [
-                ./modules/home/linux.nix
-              ];
-            };
-            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
-	          home-manager.backupFileExtension = "backup";
-          }
+          ./hosts/digitalocean/configuration.nix
         ];
       };
 
