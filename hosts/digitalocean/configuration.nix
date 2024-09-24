51d36ba655bc486966b0efe29af8dfaf2009580f
Buddha Christ <budchris@solrey3.com>
2024-09-23 22:50:48 -0400

    update digitalocean configuration

--- hosts/digitalocean/configuration.nix
+++ hosts/digitalocean/configuration.nix
@@ -32,6 +32,25 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINNTDemhkFYx8kw6p096XBVp7H2gnONZLMX+4uDgwue/ budchris@delta"
   ];
 
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.budchris = {
+    isNormalUser = true;
+    description = "Buddha Christ";
+    extraGroups = [ "networkmanager" "wheel" "audio" "docker" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFtQcMtGB55jBNuxxvlKXfeYLhy0wsPtIVt2KorpgXhQ budchris@alpha"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFm804g6IUQO1ELH716hLjTBj3zzzbuOUbL3jCE7Gej8 budchris@bravo"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHDwRpWc0JaniB1yBdKNkS5srLkCE67y+sI4Sn+wL55L budchris@charlie"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINNTDemhkFYx8kw6p096XBVp7H2gnONZLMX+4uDgwue/ budchris@delta"
+    ];
+  };
+  
+  # Enable Flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
   swapDevices = [{
     device = "/swap/swapfile";
     size = 1024 * 2; # 2 GB
