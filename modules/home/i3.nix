015fc77c0a515f2fc86470fc50b11de5b708d1bc
Solito Reyes III <solrey3@solrey3.com>
2025-06-12 23:44:07 -0400

    feat: use dotfiles i3 config

--- modules/home/i3.nix
+++ modules/home/i3.nix
@@ -5,41 +5,10 @@
   xsession.enable = true;
   xsession.windowManager.i3 = {
     enable = true;
-    config = {
-      modifier = "Mod4"; # Windows key
-      fonts = {
-        names = [ "JetBrainsMono Nerd Font" ];
-        size = 12.0;
-      };
-
-      keybindings = {
-        "Mod4+Return" = "exec alacritty";
-        "Mod4+d" = "exec rofi -show run";
-        "Mod4+Shift+q" = "kill";
-        "Mod4+Shift+r" = "restart";
-        # CHANGE LAYOUTS:
-        "Mod4+s" = "layout stacking";  # set stacking layout
-        "Mod4+w" = "layout tabbed";    # set tabbed layout
-        "Mod4+e" = "layout toggle split"; # toggle between vertical/horizontal split
-
-        # MOVE FOCUS AROUND (optional but nice)
-        "Mod4+h" = "focus left";
-        "Mod4+j" = "focus down";
-        "Mod4+k" = "focus up";
-        "Mod4+l" = "focus right";
-      };
-
-      bars = [
-        {
-          statusCommand = "i3status";
-          fonts = {
-            names = [ "JetBrainsMono Nerd Font" ];
-            size = 12.0;
-          };
-        }
-      ];
-    };
   };
+
+  # Use configuration from the dotfiles repository
+  home.file.".config/i3/config".source = "${dotfiles}/i3/config";
 
   home.packages = with pkgs; [
     i3
