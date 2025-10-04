6e33ba54739f4c79cc20e18a92b494bb07917c68
Buddha Christ <budchris@solrey3.com>
2025-10-04 11:01:00 -0400

    chore: update flake.lock

---
+++ hosts/kilo/configuration.nix
@@ -0,0 +1,27 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./../../modules/nixos/base.nix
+    ./../../modules/nixos/xfce4.nix
+    ./../../modules/nixos/pipewire.nix
+    ./../../modules/nixos/bluetooth.nix
+    ./../../modules/nixos/avahi.nix
+    ./../../modules/nixos/docker.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "kilo"; # Define your hostname.
+
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
